e7b65eae931e661f29c0631a489cad0609c912a7
ananya <ananyajoshi005@gmail.com>
2025-07-23 16:16:21 +0530

    ccc

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -5,7 +5,7 @@ const connectDb = async () => {
     await mongoose.connect(process.env.MONGO_URL, {});
     console.log("Database connected successfully");
   } catch (error) {
-    console.log("Database connection error:", error);
+    console.log("Database MongoDB connection error:", error);
     process.exit(1);
   }
 };
