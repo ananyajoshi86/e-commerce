b4b029c4579ed057a654d75aefae93fd65a5b0fc
ananya <ananyajoshi005@gmail.com>
2025-07-23 17:38:25 +0530

    7th commit

--- frontend/src/Admin/AdminLogin.jsx
+++ frontend/src/Admin/AdminLogin.jsx
@@ -28,7 +28,7 @@ export default function AdminLogin() {
       if (res && res.success) {
         localStorage.setItem("adminToken", res.token);
         setIsLoggedIn(true);
-        navigate("/AdminDashboard");
+        navigate("/adminDashboard");
       } else {
         console.log(res);
         setIsLoggedIn(false);
